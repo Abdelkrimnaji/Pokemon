901ad6c4760ca69d1adf8586d02e8533a94df9b5
Abdelkrimnaji <48068321+Abdelkrimnaji@users.noreply.github.com>
2020-09-18 11:32:29 +0200

    Add files via upload

---
+++ build/precache-manifest.2f2939976720b6151857e8d4f68d55ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a847068a5c0be15c187c037185fafc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b2de1c06c9003ab597f",
+    "url": "/static/css/main.50bb09be.chunk.css"
+  },
+  {
+    "revision": "481caf7cc31529764a41",
+    "url": "/static/js/2.5eacfc5d.chunk.js"
+  },
+  {
+    "revision": "2b2de1c06c9003ab597f",
+    "url": "/static/js/main.6206c89d.chunk.js"
+  },
+  {
+    "revision": "a426aa9cdef82f18f533",
+    "url": "/static/js/runtime-main.cc96f8ce.js"
+  }
+]);
